5a73e12ce17b63349149f372a3242a1edfa68307
nawaz76158-maker <nmd76158@gmail.com>
2026-07-10 17:58:15 +0530

    feat: day 4 - add global logging middleware to log all incoming requests

---
+++ Day4.js
@@ -0,0 +1,19 @@
+// feat: day 4 - global logging middleware
+
+const express = require("express");
+const app = express();
+
+app.use((req, res, next) => {
+    console.log(`${req.method} request to ${req.url}`);
+    next();
+});
+
+app.get("/", (req, res) => {
+    res.status(200).send("Welcome to my express server");
+});
+
+app.get("/about", (req, res) => {
+    res.status(200).send("This is my about page");
+});
+
+app.listen(3000);
