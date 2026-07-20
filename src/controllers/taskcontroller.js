ede9b3f94d783dbb41dae4c903a1f71a30f55557
nawaz76158-maker <nmd76158@gmail.com>
2026-07-20 14:13:38 +0530

    completed the Day11 with crud operations

--- src/controllers/taskcontroller.js
+++ src/controllers/taskcontroller.js
@@ -29,12 +29,12 @@ const getTaskById = async (req, res) => {
 // POST /tasks
 const createTask = async (req, res) => {
     try {
-        const { title } = req.body;
+        const { title, description, priority, dueDate} = req.body;
         if (!title) {
             return res.status(400).json({ message: "Title is required" });
         }
     
-        const newTask = await Task.create({ title });
+        const newTask = await Task.create({ title, description, priority, dueDate });
 
         res.status(201).json({ message: "Task created successfully", data: newTask });
           } catch (error) {
@@ -49,16 +49,19 @@ const updateTask = async (req, res) => {
         const task = await Task.findById(req.params.id);
 
         if (!task) {
-            return res.status(404).json({ message: "Task not found" });
+            return res.status(404).json({
+                message: "Task not found"
+            });
         }
 
-        const { title } = req.body;
+        const { title, description, priority, dueDate, completed } = req.body;
 
-        if (!title) {
-            return res.status(400).json({ message: "Title is required for update" });
-        }
+        if (title !== undefined) task.title = title;
+        if (description !== undefined) task.description = description;
+        if (priority !== undefined) task.priority = priority;
+        if (dueDate !== undefined) task.dueDate = dueDate;
+        if (completed !== undefined) task.completed = completed;
 
-        task.title = title;
         await task.save();
 
         res.status(200).json({
@@ -67,7 +70,9 @@ const updateTask = async (req, res) => {
         });
 
     } catch (error) {
-        res.status(500).json({ message: "Error updating task" });
+        res.status(500).json({
+            message: "Error updating task"
+        });
     }
 };
 
--- src/models/Task.js
+++ src/models/Task.js
@@ -2,8 +2,11 @@ const mongoose = require("mongoose");
 
 const taskSchema = new mongoose.Schema({
   title: { type: String, required: true },
+  description: { type: String },
+  priority: { type: String , default: "Medium" },
+  dueDate: { type: Date },
   completed: { type: Boolean, default: false }
-});
+}, { timestamps: true });  // this auto-adds createdAt AND updatedAt for you
 
 const Task = mongoose.model("Task", taskSchema);
 
