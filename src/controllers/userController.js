b213e02dec17c14b36bb4dc373ebee0346ebf2f0
nawaz76158-maker <nmd76158@gmail.com>
2026-07-31 12:08:30 +0530

    feat: implement user authentication with signup and login

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,4 +1,5 @@
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 const User = require("../models/User");
 
 const signup = async (req, res) => {
@@ -50,4 +51,64 @@ const signup = async (req, res) => {
   }
 };
 
-module.exports = { signup };
+const login = async (req, res) => {
+try {
+    const { email, password } = req.body;
+    if (!email || !password) {
+    return res.status(400).json({
+        message: "Email and password are required"
+    });
+}
+
+const normalizedEmail = email.toLowerCase().trim();
+
+const user = await User.findOne({
+    email: normalizedEmail
+});
+
+if(!user){
+  return res.status(401).json({
+    message: "Invalid email or password"
+  })
+}
+ 
+const isMatch = await bcrypt.compare(password, user.password);
+
+if (!isMatch) {
+    return res.status(401).json({
+        message: "Invalid email or password"
+    });
+}
+
+const token = jwt.sign(
+    {
+        userId: user._id
+    },
+    process.env.JWT_SECRET,
+    {
+        expiresIn: "1d"
+    }
+);
+ return res.status(200).json({
+  message: "Login successful",
+  token,
+  data: {
+    id: user._id,
+    name: user.name,
+    email: user.email
+  }
+});
+
+} catch (error) {
+  console.error(error);
+
+  res.status(500).json({
+    message: "Error logging in"
+  });
+ }
+};
+
+module.exports = { 
+  signup,
+   login 
+};
